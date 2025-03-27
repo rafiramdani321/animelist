c409a7bd55a48ae40fae72481e7986f93c8af035
rafiramdani321 <rafiramdani321@gmail.com>
2025-03-28 01:37:50 +0700

    update code

--- app/(browser)/(home)/page.tsx
+++ app/(browser)/(home)/page.tsx
@@ -1,15 +1,3 @@
-import React, { Suspense } from "react";
-import PageAnimeSeasonNow from "./anime-season-now";
-import { ResultsAnimeSkeleton } from "./anime-season-now/results";
-
 export default function Page() {
-  return (
-    <div>
-      <div className="px-4 mt-[47.5rem] md:mt-[53.5rem] xl:mt-[51rem] md:px-10 xl:px-64 space-y-14">
-        <Suspense fallback={<ResultsAnimeSkeleton />}>
-          <PageAnimeSeasonNow />
-        </Suspense>
-      </div>
-    </div>
-  );
+  return <div>Hello World</div>;
 }
